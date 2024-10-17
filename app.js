8f3a5ca22417c55ced510cc64b79e5113adfd222
Avinash sharma <sharmaavi2196@gmail.com>
2024-10-17 23:43:35 +0530

    writing jsx

--- app.js
+++ app.js
@@ -1,23 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "i m a h1"),
-    React.createElement("h2", {}, "i m a h2"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "i m a h1"),
-    React.createElement("h2", {}, "i m a h2"),
-  ]),
-]);
-
 const heading = React.createElement(
   "h1",
-  { id: "heading", xyz: "abc" },
-  "namastey react with help of javaScript"
+  { id: "heading" },
+  "Namastey react 🚀👩‍🚀👨‍🚀"
 );
-console.log(parent);
+const jsxheading = <h1>namastey react in jsx</h1>;
+console.log(heading);
+console.log(jsxheading);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(jsxheading);
