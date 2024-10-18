41ec41162953cdec36552aafe387c353291fb2a3
Avinash sharma <sharmaavi2196@gmail.com>
2024-10-18 18:06:30 +0530

    Merge branch 'main' of https://github.com/AAvi09/namaskaram-react

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+//react element
 const heading = React.createElement(
   "h1",
   { id: "heading" },
   "Namastey react 🚀👩‍🚀👨‍🚀"
 );
+
 const jsxheading = <h1>namastey react in jsx</h1>;
 console.log(heading);
 console.log(jsxheading);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
+//react functional components
+const HeadingComponent = () => {
+  return <h1>Namastey react functional component</h1>;
+};
 root.render(jsxheading);
