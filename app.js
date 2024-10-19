f903f07c1109e838d0b68f06f0d8e3621ea9f9c8
Avinash sharma <sharmaavi2196@gmail.com>
2024-10-19 20:43:45 +0530

    header created of food app

--- app.js
+++ app.js
@@ -1,19 +1,46 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-//react element
-const Title = () => (
-  <h1 className="head" tabIndex="5">
-    Namastey react 🚀👩‍🚀👨‍🚀
-  </h1>
-);
-
-//react functional components
-const HeadingComponent = () => (
-  <div id="container">
-    <Title />
-    <h1>Namastey react functional component</h1>;
-  </div>
-);
+/**
+ * Header
+ * -Logo
+ * -new items
+ * Body
+ * - search
+ * - Restraunt container
+ *   - Restraunt card
+ * Footer
+ * - copyright
+ * - links
+ * - address
+ * - contact
+ */
+const Header = () => {
+  return (
+    <div className="header">
+      <div>
+        <img
+          className="logo"
+          src="https://thumbs.dreamstime.com/b/fast-food-logo-design-concepts-set-unique-ideas-pizza-kebab-burger-retro-elements-symbols-icons-46091401.jpg"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About </li>
+          <li>Contact</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+    </div>
+  );
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadingComponent />);
+root.render(<AppLayout />);
