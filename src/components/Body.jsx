2f5b88f85e5505ea2f96ba206a46189df2e2ab9d
Avinash sharma <sharmaavi2196@gmail.com>
2024-10-31 23:56:50 +0530

    used swiggy to render data in console using useEffect hook

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,10 +1,25 @@
 import React from "react";
 import Rescard from "./Rescard";
 import resList from "../utils/mockdata";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Body = () => {
   const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.5930976&lng=77.3969121&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    console.log(json);
+    setListOfRestaurants(
+      json.data.cards[1].card.gridElements.infoWithStyle.restaurants.info
+    );
+  };
   return (
     <div className="body">
       <div className="filter">
