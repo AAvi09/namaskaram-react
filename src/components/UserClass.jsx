d98acafb0e8ce9678839d3ba9561baf9aea9789d
Avinash sharma <sharmaavi2196@gmail.com>
2024-11-12 23:47:14 +0530

    changed state using setState function

--- src/components/UserClass.jsx
+++ src/components/UserClass.jsx
@@ -14,6 +14,16 @@ class UserClass extends React.Component {
       <div className="user-card">
         <h1>Count : {this.state.count}</h1>
         <h2>Count2 : {this.state.count2}</h2>
+        <button
+          onClick={() => {
+            this.setState({
+              count: this.state.count + 1,
+              count2: this.state.count + 1,
+            });
+          }}
+        >
+          Count increase
+        </button>
         <h1>Name : {this.props.name}</h1>
         <h2>twitter : avi@210966</h2>
         <h3>Location : {this.props.location}</h3>
