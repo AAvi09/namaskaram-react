c5bdad81ae5a4aefc63c299f25bc56979cebe99c
Avinash sharma <sharmaavi2196@gmail.com>
2024-11-13 23:26:01 +0530

    converted about into class based component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,15 +1,23 @@
 import Users from "./Users";
 import UserClass from "./UserClass";
+import { Component } from "react";
 
-const About = () => {
-  return (
-    <div>
-      <h1>About</h1>
-      <h2>This is swiggy clone building ......</h2>
-      <Users name={"Avinash sharma(function)"} />
-      <UserClass name={"Avi(class)"} location={"chaand pe he apun"} />
-    </div>
-  );
-};
+class About extends Component {
+  constructor(props) {
+    super(props);
+
+    console.log("parent constructor");
+  }
+  render() {
+    return (
+      <div>
+        <h1>About</h1>
+        <h2>This is swiggy clone building ......</h2>
+        <Users name={"Avinash sharma(function)"} />
+        <UserClass name={"Avi(class)"} location={"chaand pe he apun"} />
+      </div>
+    );
+  }
+}
 
 export default About;
