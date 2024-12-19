9e7fe19cfb049d8457025cf8cfd8d7bf0c8db323
Avinash sharma <sharmaavi2196@gmail.com>
2024-12-19 20:45:25 +0530

    upddated contact us page ui

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,20 @@ import React from "react";
 function Contact() {
   return (
     <div>
-      <h1>Contact us at bhaad ki bhatti📞📱☎📲</h1>
+      <h1 className="font-bold black text-center p-4 m-4">
+        Contact us 📞📱☎📲
+      </h1>
+      <form className="space-x-2">
+        <input className="borded-black p-2 m-2 " placeholder="name" />
+        <input className="borded-black p-2 m-2 " placeholder="contact number" />
+        <input
+          className="borded-black p-2 m-2 "
+          placeholder="what you wanna say"
+        />
+        <button className="bg-red-600 text-white rounded-lg p-2 m-2">
+          submit
+        </button>
+      </form>
     </div>
   );
 }
