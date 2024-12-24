1d6c8addb832f7b76874e9902375d9838249803d
Avinash sharma <sharmaavi2196@gmail.com>
2024-12-25 00:14:09 +0530

    tested clicking and changing of login button to logout

--- src/components/__tests__/Header.test.js
+++ src/components/__tests__/Header.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import Header from "../Header";
 import { Provider } from "react-redux";
 import appStore from "../../utils/appStore";
@@ -20,4 +20,23 @@ it("Should render Header component with a login button", () => {
 
   //Assertion
   expect(loginButton).toBeInTheDocument();
+});
+
+it("should change login button to logout on click", () => {
+  render(
+    <BrowserRouter>
+      <Provider store={appStore}>
+        {/* <UserContext.Provider> */}
+        <Header />
+        {/* </UserContext.Provider> */}
+      </Provider>
+    </BrowserRouter>
+  );
+  const loginButton = screen.getByRole("button", { name: "Login" });
+
+  fireEvent.click(loginButton);
+
+  const logoutButton = screen.getByRole("button", { name: "Logout" });
+
+  expect(logoutButton).toBeInTheDocument();
 });
